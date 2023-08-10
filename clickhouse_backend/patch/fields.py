bed74df8255347236107fc7698bdd07111b80df7
Filipp Ozinov <filipp@bakanov.su>
2023-08-10 18:00:40 +0400

    Suppress wrong parsing error

--- clickhouse_backend/patch/fields.py
+++ clickhouse_backend/patch/fields.py
@@ -1,5 +1,6 @@
 from django.db.models.fields import json
 
+_g_old_from_db = None
 
 def key_transform_as_clickhouse(self, compiler, connection):
     lhs, params, key_transforms = self.preprocess_lhs(compiler, connection)
@@ -14,6 +15,14 @@ def key_transform_as_clickhouse(self, compiler, connection):
             params.append(key)
     return sql, tuple(params)
 
+def patched_db_from(obj, value, expression, connection):
+    try:
+        return _g_old_from_db(obj, value, expression, connection)
+    except UnicodeDecodeError:
+        return ''
 
 def patch_jsonfield():
     json.KeyTransform.as_clickhouse = key_transform_as_clickhouse
+    global _g_old_from_db
+    _g_old_from_db = json.JSONField.from_db_value
+    json.JSONField.from_db_value = patched_db_from
